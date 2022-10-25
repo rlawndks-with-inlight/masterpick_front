ec421ce2d91b7ceeb3e797a9fb38d7aa1cbd08e8
rlawndks4 <rlawndks0423@naver.com>
2022-10-25 18:45:58 +0900

    asfd

--- src/pages/User/Home.js
+++ src/pages/User/Home.js
@@ -141,6 +141,18 @@ const Home = () => {
             setTimeout(() => setLoading(false), 1500);
         }
         fetchPost();
+        async function isLogined(){
+            await window.flutter_inappwebview.callHandler('native_app_logined',{}).then(async function (result) {
+                //result = "{'code':100, 'message':'success', 'data':{'login_type':1, 'id': 1000000}}"
+                // JSON.parse(result)
+
+                let obj = JSON.parse(result);
+                await onLoginBySns(obj.data);
+            });
+        }
+        if (window && window.flutter_inappwebview) {
+            isLogined();
+        }
     }, [])
     const getListByYieldList = (list) => {
         let result = list;
@@ -154,6 +166,29 @@ const Home = () => {
             return parseInt(a.yield_sequence) - parseInt(b.yield_sequence)
         })
         return result;
+    }
+    const onLoginBySns = async (obj) => {
+        let nick = "";
+        if (obj.login_type == 1) {
+            nick = "카카오" + new Date().getTime();
+        } else if (obj.login_type == 2) {
+            nick = "네이버" + new Date().getTime();
+        }
+        let objs = {
+            id: obj.id,
+            name: obj.legal_name,
+            nickname: nick,
+            phone: obj.phone_number,
+            user_level: 0,
+            typeNum: obj.login_type,
+            profile_img: obj.profile_image_url
+        }
+        const { data: response } = await axios.post('/api/loginbysns', objs);
+        if (response.result > 0) {
+            await localStorage.setItem('auth', JSON.stringify(response.data));
+        } else {
+            //alert(response.message);
+        }
     }
     return (
         <>
